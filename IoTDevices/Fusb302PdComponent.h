912bb041788b3e29860a8288329edb41b97ba3f2
Richard Lin <elpato25@gmail.com>
2023-10-30 14:20:08 -0700

    broken USB PD wip

---
+++ IoTDevices/Fusb302PdComponent.h
@@ -0,0 +1,27 @@
+#include "esphome.h"
+#include "Fusb302.h"
+
+
+class Fusb302PdComponent : public PollingComponent, public Sensor {
+public:
+  Fusb302PdComponent() : PollingComponent(100), fusb_(Wire) {
+  }
+
+  float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
+
+  void setup() override {
+  }
+
+  void update() override {
+    uint8_t id = 0;
+    if (fusb_.readId(id)) {
+      publish_state(id);
+      ESP_LOGI("Fusb302Component", "got chip id %i", id);
+    } else {
+      ESP_LOGW("Fusb302Component", "failed to read chip id");
+    }
+  }
+
+protected:
+  Fusb302 fusb_;
+};
