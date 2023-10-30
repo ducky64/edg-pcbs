76981b4e13ac9f135d7834d3d080a830166fa204
Richard Lin <elpato25@gmail.com>
2023-10-30 16:01:20 -0700

    wip def broken

--- IoTDevices/Fusb302PdComponent.h
+++ IoTDevices/Fusb302PdComponent.h
@@ -1,27 +1,34 @@
 #include "esphome.h"
+#include "esphome/components/text_sensor/text_sensor.h"
 #include "Fusb302.h"
 
 
-class Fusb302PdComponent : public PollingComponent, public Sensor {
+class Fusb302PdComponent : public PollingComponent {
 public:
+  Sensor sensor_id;
+  text_sensor::TextSensor sensor_status;
+
   Fusb302PdComponent() : PollingComponent(1000), fusb_(Wire) {
   }
 
   float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
 
   void setup() override {
+    if (fusb_.readId(id_)) {
+      sensor_id.publish_state(id_);
+      ESP_LOGI("Fusb302Component", "got chip id 0x%02x", id_);
+    } else {
+      ESP_LOGW("Fusb302Component", "failed to read chip id");
+      mark_failed();
+    }
   }
 
   void update() override {
-    uint8_t id = 0;
-    if (fusb_.readId(id)) {
-      publish_state(id);
-      ESP_LOGI("Fusb302Component", "got chip id %i", id);
-    } else {
-      ESP_LOGW("Fusb302Component", "failed to read chip id %i", id);
-    }
+    
+
   }
 
 protected:
   Fusb302 fusb_;
+  uint8_t id_;  // device id, if read successful
 };
