3e8f951a362ac6f1c94064b4126372ad6e4e928f
Richard Lin <elpato25@gmail.com>
2023-10-30 16:59:14 -0700

    PROGRESS

--- IoTDevices/custom_components/fusb302/sensor.py
+++ IoTDevices/custom_components/fusb302/sensor.py
@@ -6,19 +6,18 @@ from . import Fusb302Component, CONF_HUB_ID
 
 DEPENDENCIES = ['fusb302']
 
-CONFIG_SCHEMA = sensor.sensor_schema(unit_of_measurement=UNIT_EMPTY, accuracy_decimals=1).extend({
-    cv.GenerateID(CONF_HUB_ID): cv.use_id(Fusb302Component)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(CONF_HUB_ID): cv.use_id(Fusb302Component),
     cv.Optional('id'): sensor.sensor_schema(
       unit_of_measurement=UNIT_EMPTY
     ),
-}).extend(cv.COMPONENT_SCHEMA)
+})
 
 
 async def setup_conf(config, key, hub):
     if sensor_config := config.get(key):
-        sens = await text_sensor.new_text_sensor(sensor_config)
-        cg.add(getattr(hub, f"set_{key}_text_sensor")(sens))
-
+        sens = await sensor.new_sensor(sensor_config)
+        cg.add(getattr(hub, f"set_{key}_sensor")(sens))
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_BME680_BSEC_ID])
--- IoTDevices/custom_components/fusb302/text_sensor.py
+++ IoTDevices/custom_components/fusb302/text_sensor.py
@@ -6,18 +6,18 @@ from . import Fusb302Component, CONF_HUB_ID
 
 DEPENDENCIES = ['fusb302']
 
-CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend({
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_HUB_ID): cv.use_id(Fusb302Component),
     cv.Optional('status'): text_sensor.text_sensor_schema(
-      icon=ICON_ACCURACY
+      icon="mdi:checkbox-marked-circle-outline"
     ),
 }).extend(cv.COMPONENT_SCHEMA)
+
 
 async def setup_conf(config, key, hub):
     if sensor_config := config.get(key):
         sens = await text_sensor.new_text_sensor(sensor_config)
         cg.add(getattr(hub, f"set_{key}_text_sensor")(sens))
-
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_BME680_BSEC_ID])
