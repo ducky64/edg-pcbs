4aefc65a5e7e0f93052611969ddc89d136424335
Richard Lin <elpato25@gmail.com>
2023-11-02 15:00:35 -0700

    well c6 still isn't fully supported =(

--- IoTDisplay/IoTDisplay.cpp
+++ IoTDisplay/IoTDisplay.cpp
@@ -1,22 +1,4 @@
-// Requires the GxEPD2 library: https://github.com/ZinggJM/GxEPD2
-// (available on Arduino library manager)
-
-// "spi=SPI2",
-// "spi.sck=GPIO40, 33",
-// "spi.mosi=GPIO41, 34",
-// "ledr=GPIO7, 7",
-// "ledg=GPIO15, 8",
-// "ledb=GPIO16, 9",
-// "sw0=GPIO8, 12",
-// "sw1=GPIO18, 11",
-// "sw2=GPIO17, 10",
-// "oled_rst=GPIO2, 38",
-// "oled_dc=GPIO39, 32",
-// "oled_cs=GPIO38, 31",
-// "epd_busy=GPIO42, 35",
-// "0=USB",
-// "0.dp=GPIO20, 14",
-// "0.dm=GPIO19, 13"
+// ESP32-S3 variant
 const int kSw0Pin = 8;
 const int kSw1Pin = 18;
 const int kSw2Pin = 17;
@@ -27,6 +9,27 @@ const int kEpdBusyPin = 42;
 
 const int kOledSckPin = 40;
 const int kOledMosiPin = 41;
+
+const int kLedR = 7;
+const int kLedG = 15;
+const int kLedB = 16;
+
+// ESP32-C6 variant
+const int kSw0Pin = 11;
+const int kSw1Pin = 10;
+const int kSw2Pin = 8;
+const int kOledRstPin = 3;
+const int kOledDcPin = 22;
+const int kOledCsPin = 21;
+const int kEpdBusyPin = 15;
+
+const int kOledSckPin = 23;
+const int kOledMosiPin = 20;  // NOTE - needs a jumper, otherwise pad on PCB is NC!
+
+const int kLedR = 7;
+const int kLedG = 0;
+const int kLedB = 1;
+
 
 #include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
@@ -41,34 +44,12 @@ const int kOledMosiPin = 41;
 // GxEPD2_BW<GxEPD2_270, GxEPD2_270::HEIGHT> display(GxEPD2_270(kOledCsPin, kOledDcPin, kOledRstPin, kEpdBusyPin)); // GDEW027W3 176x264, EK79652 (IL91874)
 // GxEPD2_3C<GxEPD2_270c, GxEPD2_270c::HEIGHT> display(GxEPD2_270c(kOledCsPin, kOledDcPin, kOledRstPin, kEpdBusyPin)); // GDEW027C44 176x264, IL91874
 
-GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> display(GxEPD2_290_C90c(kOledCsPin, kOledDcPin, kOledRstPin, kEpdBusyPin));  // SSD1680, compatible w/ ER-EPD-029-2R
+GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> display(GxEPD2_290_C90c(kOledCsPin, kOledDcPin, kOledRstPin, kEpdBusyPin));  // SSD1680, compatible w/ ER-EPD029-2R
 
 SPIClass hspi(HSPI);  // for ESP32
 
 
-const int kLedR = 7;
-const int kLedG = 15;
-const int kLedB = 16;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(kLedR, OUTPUT);
-  pinMode(kLedG, OUTPUT);
-  pinMode(kLedB, OUTPUT);
-
-  digitalWrite(kLedR, 1);
-  digitalWrite(kLedG, 0);
-  digitalWrite(kLedB, 0);
-
-  hspi.begin(kOledSckPin, -1, kOledMosiPin, -1);
-  display.epd2.selectSPI(hspi, SPISettings(4000000, MSBFIRST, SPI_MODE0));
-  display.init(115200);
-  digitalWrite(kLedR, 0);
-  einkHelloWorld();
-  digitalWrite(kLedG, 1);
-
-  display.hibernate();
-}
 
 const char kHelloWorld[] = "Hello World!";
 
@@ -90,6 +71,26 @@ void einkHelloWorld() {
     display.print(kHelloWorld);
   }
   while (display.nextPage());
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(kLedR, OUTPUT);
+  pinMode(kLedG, OUTPUT);
+  pinMode(kLedB, OUTPUT);
+
+  digitalWrite(kLedR, 1);
+  digitalWrite(kLedG, 0);
+  digitalWrite(kLedB, 0);
+
+  hspi.begin(kOledSckPin, -1, kOledMosiPin, -1);
+  display.epd2.selectSPI(hspi, SPISettings(4000000, MSBFIRST, SPI_MODE0));
+  display.init(115200);
+  digitalWrite(kLedR, 0);
+  einkHelloWorld();
+  digitalWrite(kLedG, 1);
+
+  display.hibernate();
 }
 
 void loop() {
