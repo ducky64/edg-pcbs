124173913354685e746d13d98eddf78606db1566
Richard Lin <richard.lin@berkeley.edu>
2023-11-21 17:28:13 -0800

    somewhat untested streaming controller

--- Crawlerbot/CrawlerbotArduinoTest/main.cpp
+++ Crawlerbot/CrawlerbotArduinoTest/main.cpp
@@ -53,16 +53,21 @@ Ssd1357 Oled;
 PwmCoprocessor Stm32;
 
 
+const size_t kNumServos = 14;
+
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 const char *kWifiSsid = "RobotDriverAP";
 // const char *password = "password";
 WiFiServer Server(80);
-
+// note, send rate over network determined by kFbReadMs
+WiFiClient Client;
 
 const int kCmdDurationMs = 1000;
-int cmdServos[10] = {0};  // remote servos
+const size_t kNumRemoteServos = 10;
+uint16_t remoteServoVaues[kNumRemoteServos] = {0};  // remote servos
 int cmdExpireMillis = 0;
 
 
@@ -88,100 +93,63 @@ void setup() {
 
   // Oled.begin();
 
-  WiFi.softAP(kWifiSsid);
-  Serial.println(WiFi.softAPIP());
+  // WiFi.softAP(kWifiSsid);
+  // Serial.println(WiFi.softAPIP());
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("lemur2", "nope");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println(WiFi.localIP());
+
   Server.begin();
+  Client = Server.available();  // initialize persistent client object
 }
 
 
 int servoUpdateIndex = 0;
-int lastServoValueMillis = 0;
+int nextRemoteServoMillis = 0;
 const int kServoUpdateMs = 100;
 const int kServoNeutral = 114;
 
-int lastFbReadMillis = 0;
+int nextFbReadMillis = 0;
 const int kFbReadMs = 50;
 
 
 void loop() {
-  Serial.println("Loop\r\n");
+  int thisMillis = millis();
 
-  WiFiClient client = Server.available();   // listen for incoming clients
-  if (client) {
-    Serial.println("New client");
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("<a href=\"/fwd\">FWD</a><br>");
-            client.print("<a href=\"/left\">LEFT</a><br>");
-            client.print("<a href=\"/right\">RIGHT</a><br>");
-            client.print("<a href=\"/back\">BACK</a><br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        int thisMillis = millis();
-        if (currentLine.endsWith("GET /fwd")) {
-          cmdServos[2] = 255;  // front left
-          cmdServos[6] = 0;  // front right
-          cmdServos[8] = 255;  // back left
-          cmdServos[9] = 0;  // back right
-          cmdExpireMillis = thisMillis + kCmdDurationMs;
-        } else if (currentLine.endsWith("GET /left")) {
-          cmdServos[2] = kServoNeutral;
-          cmdServos[6] = 0;
-          cmdServos[8] = kServoNeutral;
-          cmdServos[9] = 0;
-          cmdExpireMillis = thisMillis + kCmdDurationMs;
-        } else if (currentLine.endsWith("GET /right")) {
-          cmdServos[2] = 255;
-          cmdServos[6] = kServoNeutral;
-          cmdServos[8] = 255;
-          cmdServos[9] = kServoNeutral;
-          cmdExpireMillis = thisMillis + kCmdDurationMs;
-        } else if (currentLine.endsWith("GET /back")) {
-          cmdServos[2] = 0;
-          cmdServos[6] = 255;
-          cmdServos[8] = 0;
-          cmdServos[9] = 255;
-          cmdExpireMillis = thisMillis + kCmdDurationMs;
-        }
-      }
+  if (!Client) {
+    Client = Server.available();
+    if (Client) {
+      Serial.println("New client");
     }
-    // close the connection:
-    client.stop();
   }
 
-  int thisMillis = millis();
-  if (thisMillis - lastServoValueMillis >= kServoUpdateMs) {
+  if (Client) {
+    uint16_t cmdValues[kNumServos] = {0};
+    if (Client.available() >= sizeof(cmdValues)) {
+      Client.read((uint8_t*)cmdValues, sizeof(cmdValues));
+      for (size_t i=0; i<kNumRemoteServos; i++) {
+        remoteServoVaues[i] = cmdValues[i+4];  // first 4 are on ESP32 itself
+      }
+      cmdExpireMillis = thisMillis + kCmdDurationMs;  // always based off current millis
+    }    
+  }
+
+  if (thisMillis >= nextRemoteServoMillis) {
     digitalWrite(kPinLed, 1);
+
     for (int i=0; i<10; i++) {
       if (thisMillis < cmdExpireMillis) {
-        Stm32.writeServoValue(i, cmdServos[i]);
+        Stm32.writeServoValue(i, remoteServoVaues[i]);
       } else {  // expired, write neutral
         Stm32.writeServoValue(i, kServoNeutral);
       }
     }
+
     digitalWrite(kPinLed, 0);
 
     if (thisMillis < cmdExpireMillis) {
@@ -189,21 +157,23 @@ void loop() {
     } else {
       NeoPixels.SetPixelColor(4, RgbColor(127, 0, 0));
     }
-
     NeoPixels.Show();
 
-    // servoUpdateIndex++;
-    lastServoValueMillis = lastServoValueMillis + kServoUpdateMs;
+    nextRemoteServoMillis += kServoUpdateMs;  // increment for long term frequency stability
   }
 
-  // if (thisMillis - lastFbReadMillis >= kFbReadMs) {
-  //   uint16_t fbValue;
-  //   if (Stm32.readServoFeedback(0, &fbValue)) {
-  //     uint8_t ledValue = fbValue / 256;
-  //     NeoPixels.SetPixelColor(4, RgbColor(ledValue, ledValue, ledValue));
-  //   } else {
-  //     NeoPixels.SetPixelColor(4, RgbColor(0, 0, 0));
-  //   }
-  //   NeoPixels.Show();
-  // }
+  if (thisMillis >= nextFbReadMillis) {
+    uint16_t fbValues[kNumServos] = {0};
+    for (size_t i=0; i<kNumServos; i++) {
+      if (!Stm32.readServoFeedback(i, fbValues + i)) {
+        fbValues[i] = 0;
+      }
+    }
+
+    if (Client) {
+      Client.write((uint8_t*)fbValues, sizeof(fbValues));
+    }
+
+    nextFbReadMillis += kFbReadMs;  // increment for long term frequency stability
+  }
 }
