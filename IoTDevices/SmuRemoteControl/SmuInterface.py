6fd887becabd6bc16fc440652a2675426da47b57
Richard Lin <elpato25@gmail.com>
2024-01-30 12:20:47 -0800

    smu rest api wip

---
+++ IoTDevices/SmuRemoteControl/SmuInterface.py
@@ -0,0 +1,62 @@
+from typing import Tuple
+
+import requests
+import decimal
+
+class SmuInterface:
+  kNameMeasCurrent = 'UsbSMU Meas Current'
+  kNameMeasVoltage = 'UsbSMU Meas Voltage'
+  kNameAdcCurrent = 'UsbSMU Meas ADC Current'
+  kNameAdcVoltage = 'UsbSMU Meas ADC Voltage'
+
+  kNameSetCurrentMin = 'UsbSMU Set Current Min'
+  kNameSetCurrentMax = 'UsbSMU Set Current Max'
+  kNameSetVoltage = 'UsbSMU Set Voltage'
+  kNameEnable = 'UsbSMU Driver Enable'
+
+  @staticmethod
+  def _webapi_name(name: str) -> str:
+    # TODO should actually replace all non-alphanumeric but this is close enough
+    return name.replace(' ', '_').lower()
+
+  def _set(self, service: str, name: str, value: float) -> None:
+    resp = requests.post(f'http://{self.addr}/{service}/{self._webapi_name(name)}/set',
+                         json={'value': value})
+    if resp.status_code != 200:
+      raise Exception(f'Request failed: {resp.status_code}')
+
+  def _get(self, service: str, name: str) -> decimal.Decimal:
+    resp = requests.get(f'http://{self.addr}/{service}/{self._webapi_name(name)}')
+    if resp.status_code != 200:
+      raise Exception(f'Request failed: {resp.status_code}')
+    return decimal.Decimal(resp.json()['state'].split(' ')[0])
+
+  def __init__(self, addr: str):
+    self.addr = addr
+
+  def set_voltage(self, voltage: float) -> None:
+    self._set('number', self.kNameSetVoltage, voltage)
+
+  def set_current_limits(self, current_min: float, current_max: float) -> None:
+    assert current_min < current_max
+    self._set('number', self.kNameSetCurrentMin, current_min)
+    self._set('number', self.kNameSetCurrentMax, current_max)
+
+  def get_voltage_current(self) -> Tuple[decimal.Decimal, decimal.Decimal]:
+    """Returns the measured voltage and current"""
+    return (self._get('sensor', self.kNameMeasVoltage),
+            self._get('sensor', self.kNameMeasCurrent))
+
+  def get_raw_voltage_current(self) -> Tuple[int, int]:
+    """Returns the voltage and current ADC counts"""
+    return (int(self._get('sensor', self.kNameAdcVoltage)),
+            int(self._get('sensor', self.kNameAdcCurrent)))
+
+  def enable(self, on: bool=True) -> None:
+    if on:
+      action = 'turn_on'
+    else:
+      action = 'turn_off'
+    resp = requests.post(f'http://{self.addr}/switch/{self._webapi_name(self.kNameEnable)}/{action}')
+    if resp.status_code != 200:
+      raise Exception(f'Request failed: {resp.status_code}')
--- IoTDevices/SmuRemoteControl/cal.py
+++ IoTDevices/SmuRemoteControl/cal.py
@@ -1,12 +1,9 @@
+import argparse
 import itertools
-from socket import socket
-
-import aioesphomeapi
-import asyncio
-import datetime
-import logging
+import time
 import csv
-import sys
+
+from SmuInterface import SmuInterface
 
 
 kOutputFile = 'calibration.csv'
@@ -26,7 +23,7 @@ kSetData = [
 
 # Calibration with external reference
 # ask_user_reference = True
-# calibration_points = [  # as kSetData tuples
+# calibration_points = [  # as voltage, current min, current max
 #   # unloaded
 #   (0.0, -1, 1),
 #   (1.0, -1, 1),
@@ -82,100 +79,99 @@ calibration_points = [  # as kSetData tuples
 # ]
 
 
-state_queues: dict[int, asyncio.Queue[aioesphomeapi.SensorState]] = {}
-
-def change_callback(state):
-  """Print the state changes of the device.."""
-  # print(f"{datetime.datetime.now().time()}: {state}")
-  if isinstance(state, aioesphomeapi.SensorState):
-    state_queue = state_queues.get(state.key, None)
-    if state_queue is not None:
-      if not state_queue.full():
-        state_queue.put_nowait(state)
-
-async def get_next_states(*keys: int) -> list[aioesphomeapi.SensorState]:
-  """Get the next state for a given key."""
-  key_queues = []
-  for key in keys:
-    state_queue = state_queues.get(key, None)
-    if state_queue is None:
-      state_queue = asyncio.Queue(maxsize=1)
-      state_queues[key] = state_queue
-    key_queues.append(state_queue)
-
-  for key_queue in key_queues:
-    while not key_queue.empty():
-      await key_queue.get()  # flush prior entries
-
-  values = []
-  for key_queue in key_queues:
-    values.append(await key_queue.get())
-  return values
-
-
-async def main():
-  """Connect to an ESPHome device and get details."""
-
-  # Establish connection
-  api = aioesphomeapi.APIClient("ducky-iotusbsmu-565574.lan", port=6053, password=None)
-  await api.connect(login=True)
-  # logging.basicConfig(level=logging.DEBUG)
-  # api.set_debug(True)  # must be set after connection is made
-
-  # Get API version of the device's firmware
-  print(api.api_version)
-
-  # Show device details
-  device_info = await api.device_info()
-  print(device_info)
-
-  # List all entities of the device
-  sensors, services = await api.list_entities_services()
-  keys_by_name = dict((sensor.name, sensor.key) for sensor in sensors)
-  print(sensors)
-  print(services)
-
-
-  await api.number_command(keys_by_name["UsbSMU Set Current Max"], 0.5)
-  await api.number_command(keys_by_name["UsbSMU Set Current Min"], -0.1)
-
-  await api.subscribe_states(change_callback)
-
-
-  with open(kOutputFile, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    csvwriter.writerow(kSetData + kRecordData)
-    csvfile.flush()
-
-    for calibration_point in calibration_points:
-      for set_name, set_value in zip(kSetData, calibration_point):
-        await api.number_command(keys_by_name[set_name], set_value)
-      await asyncio.sleep(0.1)
-
-      await get_next_states(*[keys_by_name[record_name] for record_name in kRecordData])  # discard prior sample
-
-      values = [state.state for state in
-        await get_next_states(*[keys_by_name[record_name] for record_name in kRecordData])]
-
-      print(f"{calibration_point} => {values}: ")
-      if ask_user_reference:
-        user_data = await asyncio.to_thread(sys.stdin.readline)
-        user_data_split = [elt.strip() for elt in user_data.split(',')]
-      else:
-        user_data_split = []
-
-      csvwriter.writerow(list(calibration_point) + values + user_data_split)
-      csvfile.flush()
+# state_queues: dict[int, asyncio.Queue[aioesphomeapi.SensorState]] = {}
+#
+# def change_callback(state):
+#   """Print the state changes of the device.."""
+#   # print(f"{datetime.datetime.now().time()}: {state}")
+#   if isinstance(state, aioesphomeapi.SensorState):
+#     state_queue = state_queues.get(state.key, None)
+#     if state_queue is not None:
+#       if not state_queue.full():
+#         state_queue.put_nowait(state)
+#
+# async def get_next_states(*keys: int) -> list[aioesphomeapi.SensorState]:
+#   """Get the next state for a given key."""
+#   key_queues = []
+#   for key in keys:
+#     state_queue = state_queues.get(key, None)
+#     if state_queue is None:
+#       state_queue = asyncio.Queue(maxsize=1)
+#       state_queues[key] = state_queue
+#     key_queues.append(state_queue)
+#
+#   for key_queue in key_queues:
+#     while not key_queue.empty():
+#       await key_queue.get()  # flush prior entries
+#
+#   values = []
+#   for key_queue in key_queues:
+#     values.append(await key_queue.get())
+#   return values
+#
+#
+# async def main():
+#   """Connect to an ESPHome device and get details."""
+#
+#   # Establish connection
+#   api = aioesphomeapi.APIClient("ducky-iotusbsmu-565574.lan", port=6053, password=None)
+#   await api.connect(login=True)
+#   # logging.basicConfig(level=logging.DEBUG)
+#   # api.set_debug(True)  # must be set after connection is made
+#
+#   # Get API version of the device's firmware
+#   print(api.api_version)
+#
+#   # Show device details
+#   device_info = await api.device_info()
+#   print(device_info)
+#
+#   # List all entities of the device
+#   sensors, services = await api.list_entities_services()
+#   keys_by_name = dict((sensor.name, sensor.key) for sensor in sensors)
+#   print(sensors)
+#   print(services)
+#
+#
+#   await api.number_command(keys_by_name["UsbSMU Set Current Max"], 0.5)
+#   await api.number_command(keys_by_name["UsbSMU Set Current Min"], -0.1)
+#
+#   await api.subscribe_states(change_callback)
+#
+#
+#   with open(kOutputFile, 'w', newline='') as csvfile:
+#     csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+#     csvwriter.writerow(kSetData + kRecordData)
+#     csvfile.flush()
+#
+#     for calibration_point in calibration_points:
+#       for set_name, set_value in zip(kSetData, calibration_point):
+#         await api.number_command(keys_by_name[set_name], set_value)
+#       await asyncio.sleep(0.1)
+#
+#       await get_next_states(*[keys_by_name[record_name] for record_name in kRecordData])  # discard prior sample
+#
+#       values = [state.state for state in
+#         await get_next_states(*[keys_by_name[record_name] for record_name in kRecordData])]
+#
+#       print(f"{calibration_point} => {values}: ")
+#       if ask_user_reference:
+#         user_data = await asyncio.to_thread(sys.stdin.readline)
+#         user_data_split = [elt.strip() for elt in user_data.split(',')]
+#       else:
+#         user_data_split = []
+#
+#       csvwriter.writerow(list(calibration_point) + values + user_data_split)
+#       csvfile.flush()
 
 
 if __name__ == "__main__":
-  loop = asyncio.get_event_loop()
-  try:
-    # asyncio.ensure_future(main())
-    # loop.run_forever()
+  parser = argparse.ArgumentParser(prog='SmuCal')
+  parser.add_argument('addr', type=str)
+  args = parser.parse_args()
 
-    loop.run_until_complete(main())
-  except KeyboardInterrupt:
-    pass
-  finally:
-    loop.close()
+  smu = SmuInterface(args.addr)
+  print(smu.get_voltage_current())
+  smu.enable(True)
+  time.sleep(1)
+  smu.enable(False)
