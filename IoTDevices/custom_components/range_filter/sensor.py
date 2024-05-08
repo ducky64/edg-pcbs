1dd9cb83348face368c4ae5275db99b9d8235e49
Richard Lin <elpato25@gmail.com>
2024-05-08 16:02:25 -0700

    filters maybe?

--- IoTDevices/custom_components/range_filter/sensor.py
+++ IoTDevices/custom_components/range_filter/sensor.py
@@ -3,6 +3,8 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID, CONF_WINDOW_SIZE, CONF_SEND_EVERY
 
+from esphome.components.sensor import Filter, FILTER_REGISTRY
+
 range_filter_ns = cg.esphome_ns.namespace("range_filter")
 RangeFilterSensor = range_filter_ns.class_(
     "RangeFilterSensor", sensor.Sensor, cg.Component,
@@ -30,3 +32,23 @@ async def to_code(config):
     )
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
+
+
+RangeFilter = range_filter_ns.class_("RangeFilter", Filter)
+
+RANGE_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.Optional(CONF_WINDOW_SIZE, default=5): cv.positive_not_null_int,
+            cv.Optional(CONF_SEND_EVERY, default=5): cv.positive_not_null_int,
+        }
+    ),
+)
+
+@FILTER_REGISTRY.register("range", RangeFilter, RANGE_SCHEMA)
+async def min_filter_to_code(config, filter_id):
+    return cg.new_Pvariable(
+        filter_id,
+        config[CONF_WINDOW_SIZE],
+        config[CONF_SEND_EVERY],
+    )
